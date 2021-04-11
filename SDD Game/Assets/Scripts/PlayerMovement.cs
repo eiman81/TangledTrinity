27aff58f403532cb18f908c2070ff53244a4c67a
mcrekt1 <mcrekt1@gmail.com>
2021-04-11 14:55:28 +1000

    Added project

---
+++ SDD Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+
+    void Update()
+    {
+        float h, v;
+
+        h = Input.GetAxis ("Horizontal");
+        v = Input.GetAxis ("Vertical");
+    }
+}
