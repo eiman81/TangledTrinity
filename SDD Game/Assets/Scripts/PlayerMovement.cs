5fe58f9bd1b1577b935b3328a10d64caaaebec9f
mcrekt1 <82092732+mcrekt1@users.noreply.github.com>
2021-04-23 12:51:12 +1000

    Update PlayerMovement.cs

--- SDD Game/Assets/Scripts/PlayerMovement.cs
+++ SDD Game/Assets/Scripts/PlayerMovement.cs
@@ -2,50 +2,18 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float Speed, jumpPower;
-
-    Rigidbody rb;
-
-    float distToGround;
-
-   
-    public Transform sphere;
-    bool Grounded;
-
-    void Start ()
-    {
-        distToGround = GetComponent<Collider> ().bounds.extents.y;
-
-        rb = GetComponent<Rigidbody> ();
-    }
-
+    public float Speed;
+  
     void FixedUpdate ()
     {
         PlayerMove ();
-        Debug.Log (Grounded);
-        Grounded = Physics.CheckSphere (sphere.position, 0.1f);
     }
-
 
     void PlayerMove ()
     {
         float h, v;
-
-        if (Grounded)
-        {
             h = Input.GetAxis ("Horizontal");
             v = Input.GetAxis ("Vertical");
-
-            if (Input.GetKeyDown (KeyCode.Space))
-            {
-                rb.AddForce (Vector3.up * jumpPower, ForceMode.VelocityChange);          
-            }
-        }
-        else
-        {
-            h = Input.GetAxis ("Horizontal") / 2;
-            v = Input.GetAxis ("Vertical") / 2;
-        }
 
         Vector3 movement = Quaternion.Euler (0, Camera.main.transform.eulerAngles.y, 0) * new Vector3 (h * Speed * Time.deltaTime, 0, v * Speed * Time.deltaTime);
 
