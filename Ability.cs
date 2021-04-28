dd6c16ac3c102debc52177b38cd4dc75cc3bf258
mcrekt1 <82092732+mcrekt1@users.noreply.github.com>
2021-04-28 10:01:50 +1000

    Add files via upload

---
+++ Ability.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ability : ScriptableObject
+{
+    public int cooldown;
+    public int amount;
+
+    public virtual void Use(int amount)
+    {
+
+    }
+
+    [CreateAssetMenu(menuName = "Ability")]
+    public class Fireball : Ability
+    {
+        public override void Use(int amount)
+        {
+            
+        }
+    }
+}
---
+++ AbilityController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityController : MonoBehaviour
+{
+    public Ability[] ability;
+    PlayerStats stats;
+
+    void Awake()
+    {
+        stats = GetComponent<PlayerStats>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+
+        }
+    }
+}
